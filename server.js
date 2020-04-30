5f2656507c1ce017650ae9584828fc48053f6a7f
Anant Bahuguna <anantbahugunaddn@gmail.com>
2020-04-30 11:31:52 +0530

    Setup MongoDB connection

--- server.js
+++ server.js
@@ -1,21 +1,26 @@
-const express = require('express')
+const express = require("express");
+const connectDB = require("./config/db");
 
+const app = express();
 
-const app = express()
+//Connect DB
+connectDB();
 
-app.get("/",(req,res)=> {
-    res.json({msg: "Welcome to contact keeper api"})
-})
+//Init middleware
+app.use(express.json({ extended: false }));
+//Now we can accept body data in the form of json
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Welcome to contact keeper api" });
+});
 
 //Define Routes
-app.use('/api/users', require('./routes/users'))
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/contacts', require('./routes/contacts'))
-
-
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,() => {
-    console.log(`SERVER started on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`SERVER started on port ${PORT}`);
+});
